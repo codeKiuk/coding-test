1bc935eef4ac092f524957fb01c3e5a1eb5920fc
codeKiuk <ugiya0628@icloud.com>
2024-03-13 21:23:52 +0900

    이진 탐색 문제 풀이 틀린 거 고치기

--- 2024_03_13_이진탐색/main.py
+++ 2024_03_13_이진탐색/main.py
@@ -1,43 +1,45 @@
 # 떡볶이 떡 만들기
 # 파라메트릭 서치.. 최적화문제를 결정 문제로 바꾼다
 # 적어도!!! m만큼의 떡을 가져간다.. m보다 크거나 같으면 되는거잖아?
+# 탐색 범위가 너무 크면 (이 문제처럼 10억..) 이진탐색을 고려해보자
 def solution1():
     from bisect import bisect_right
     n, m = map(int, input().split())
 
     # 떡의 개별 높이 정보
     array = list(map(int, input().split()))
-    array.sort() # 오름차순
+    array.sort()
 
-    # 4 6cm
-    # 0 1 2 3
-
+    start = 0 # start
+    end = array[-1] # end
 
     answer = 0
 
-    def binary_search(array, start, end):
+    def binary_search(start, end, answer):
+        print(f"start: {start}, end: {end} answer: {answer}")
+
         if start >= end:
-            return None
+            return answer
 
-        middle = (start + end)//2
-        middle_value = array[middle]
-
-        left_side = array[:middle]
-        right_side = array[middle:]
 
         result = 0
+        for i in range(n):
+            if array[i] > start:
+                result += array[i] - start
+            
 
-        for i in right_side:
-            result += i - middle_value
-
+        middle = (start + end) // 2
         if result == m:
-            return middle_value
+            answer = start
+            return answer
         elif result > m:
-            return binary_search(array, middle+1, end)
+            answer = start
+            return binary_search(middle+1, end, answer)
         else:
-            return binary_search(array, start, middle-1)
+            return binary_search(start, middle, answer)
+        pass
 
-    print(binary_search(array, 0, n-1))
-    pass
+
+    print(binary_search(start, end, answer))
 
 solution1()
