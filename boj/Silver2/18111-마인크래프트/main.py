b5db49b5225c2db7fa6824d9d380a87f3220e351
codeKiuk <ugiya0628@icloud.com>
2024-07-23 14:34:56 +0900

    18111-마인크래프트

--- boj/Silver2/18111-마인크래프트/main.py
+++ boj/Silver2/18111-마인크래프트/main.py
@@ -1,57 +1,46 @@
+from collections import Counter
+
 def main():
     import sys
-    sys.setrecursionlimit(10**6)
     input = sys.stdin.readline
 
     N, M, B = map(int, input().split())
 
-    global graph
-    graph = []
-    min_height = 0
-    max_height = 0
+    graph = [list(map(int, input().split())) for _ in range(N)]
+    
+    height_counter = Counter(block for row in graph for block in row)
+    min_height = min(height_counter.keys())
+    max_height = max(height_counter.keys())
 
-    for _ in range(N):
-        graph_line = list(map(int, input().split()))
-        graph.append(graph_line)
-
-        min_height = min(min_height, min(graph_line))
-        max_height = max(max_height, max(graph_line))
-
-    # 250,000 * 256..
-    def recursion(node, blocks, target_height, current_time=0):
-        x, y = node
-        height = graph[x][y]
-
-        if height == target_height:
-            pass
-        elif height > target_height:
+    min_time = float('inf')
+    answer_height = -1
+    for target_height in range(min_height, max_height+1):
+        removed_blocks = 0
+        added_blocks = 0
+        for height, count in height_counter.items():
             gap = height - target_height
-            current_time += gap * 2
-        else:
-            gap = target_height - height
-            if gap > blocks:
-                return 500 * 500 * 256
-            else:
-                current_time += gap
-                blocks -= gap
-            
+            if gap > 0:
+                removed_blocks += gap * count
+            elif gap < 0:
+                added_blocks += -gap * count
 
-        if x == N-1 and y == M-1:
-            return current_time
-        elif y == M-1:
-            return recursion((x+1, 0), blocks, target_height, current_time)
-        else:
-            return recursion((x, y+1), blocks, target_height, current_time)
+        time = 2*removed_blocks + added_blocks
+        left_blocks = B + removed_blocks - added_blocks
+        if left_blocks < 0:
+            continue
+        if time <= min_time:
+            min_time = time
+            answer_height = target_height
 
-    min_time = 500 * 500 * 256
-    target_height = 0
-    for i in range(min_height, max_height+1):
-        possible_min = recursion((0,0), B, target_height=i)
-        if min_time >= possible_min:
-            min_time = possible_min
-            target_height = i
-
-    print(f"{min_time} {target_height}")
+    print(f"{min_time} {answer_height}")
 
 
 main()
+
+
+"""
+1 7 10
+65 64 64 64 63 62 0
+
+148 56
+"""
