3701e6dbd408e351cbba5c4a68d2a894b552d127
codeKiuk <ugiya0628@icloud.com>
2024-07-29 15:22:11 +0900

    1182-부분수열의합 다른풀이

--- boj/Silver2/1182-부분수열의합/main.py
+++ boj/Silver2/1182-부분수열의합/main.py
@@ -1,6 +1,40 @@
+import sys
+
+def recursion_solution():
+    input = sys.stdin.readline
+    N, S = map(int, input().split())
+    numbers = list(map(int, input().split()))
+    answer = 0
+
+    def recursion(length, nodes, idx=0):
+        nonlocal answer
+        nonlocal S
+        nonlocal N
+        nonlocal numbers
+
+        if len(nodes) == length:
+            if sum(nodes) == S:
+                answer += 1
+            return
+
+        if idx == N:
+            return
+
+        for index in range(idx, N):
+            node = numbers[index]
+            nodes.append(node)
+            recursion(length, nodes, index+1)
+            nodes.pop()
+
+    for i in range(1, N+1):
+        recursion(i, [])
+
+    print(answer)
+
+recursion_solution()
+
+from itertools import combinations
 def main():
-    import sys
-    from itertools import combinations
     input = sys.stdin.readline
 
     N, S = map(int, input().split())
@@ -14,4 +48,3 @@ def main():
                 answer += 1
 
     print(answer)
-main()
