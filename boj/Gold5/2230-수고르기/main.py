515ce6824f7891cf478c2d0d265d9d407bf5b1f3
codeKiuk <ugiya0628@icloud.com>
2024-08-05 16:42:39 +0900

    2230-수고르기 투포인터 풀이

--- boj/Gold5/2230-수고르기/main.py
+++ boj/Gold5/2230-수고르기/main.py
@@ -1,6 +1,6 @@
 import sys
 from bisect import bisect_left
-def main():
+def main(): # 이분탐색 풀이
     input = sys.stdin.readline
 
     N, M = map(int, input().split())
@@ -18,4 +18,26 @@ def main():
 
     print(min_diff)
 
-main()
+
+def main2(): # 투포인터 풀이# 투포인터 풀이
+    input = sys.stdin.readline
+
+    N, M = map(int, input().split())
+    A = sorted([int(input()) for _ in range(N)])
+    min_diff = float('inf')
+
+    start_idx = 0
+    end_idx = 0
+
+    while end_idx < N and start_idx < N:
+        diff = A[end_idx] - A[start_idx]
+        if diff < M:
+            end_idx += 1
+        else:
+            min_diff = min(min_diff, diff)
+            start_idx += 1
+
+    print(min_diff)
+
+
+main2()
