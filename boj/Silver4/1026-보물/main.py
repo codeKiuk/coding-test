eb22cf85ac0eb29a708346f16fe9655889ff9451
codeKiuk <ugiya0628@icloud.com>
2024-08-09 22:04:29 +0900

    1026-보물

---
+++ boj/Silver4/1026-보물/main.py
@@ -0,0 +1,13 @@
+import sys
+def main():
+    input = sys.stdin.readline
+    N = int(input())
+    A = sorted(list(map(int, input().split())), reverse=True)
+    B = sorted(list(map(int, input().split())))
+    result = 0
+    for i in range(N):
+        result += A[i] * B[i]
+
+    print(result)
+
+main()
